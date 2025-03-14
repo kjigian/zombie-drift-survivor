bf029d7e5652fb9c91e820ac847c7e4e51862f6c
kjigian <kjigian@gmail.com>
2025-03-14 15:53:05 -0400

    Add AudioManager.js - Tone.js audio system

---
+++ src/audio/AudioManager.js
@@ -0,0 +1,18 @@
+/**
+ * Handles all Tone.js audio functionality for the game
+ */
+export default class AudioManager {
+    constructor() {
+        console.log('AudioManager initialized');
+    }
+
+    init() {
+        // Will be implemented in Step 20
+        console.log('AudioManager init called');
+    }
+
+    playSound(soundId) {
+        // Will be implemented in Step 20
+        console.log(`Playing sound: ${soundId}`);
+    }
+}
