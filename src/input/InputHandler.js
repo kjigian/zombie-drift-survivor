a617d021a7728a160751cb6736e996fae9d3ef98
kjigian <kjigian@gmail.com>
2025-03-14 15:53:19 -0400

    Add InputHandler.js - keyboard and mouse controls

---
+++ src/input/InputHandler.js
@@ -0,0 +1,19 @@
+/**
+ * Handles keyboard and mouse input for the game
+ */
+export default class InputHandler {
+    constructor() {
+        this.keys = {};
+        console.log('InputHandler initialized');
+    }
+
+    init() {
+        // Will be implemented in Step 9
+        console.log('InputHandler init called');
+    }
+
+    update() {
+        // Will be implemented in Step 9
+        // console.log('Input update');
+    }
+}
