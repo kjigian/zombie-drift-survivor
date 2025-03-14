599d5b49dcbc9b6040f96d0a42569b14460557b3
kjigian <kjigian@gmail.com>
2025-03-14 15:52:53 -0400

    Add Renderer.js - Three.js renderer

---
+++ src/rendering/Renderer.js
@@ -0,0 +1,24 @@
+/**
+ * Handles all Three.js rendering functionality for the game
+ */
+export default class Renderer {
+    constructor(canvas) {
+        this.canvas = canvas;
+        console.log('Renderer initialized');
+    }
+
+    init() {
+        // Will be implemented in Step 2
+        console.log('Renderer init called');
+    }
+
+    render() {
+        // Will be implemented in Step 2
+        // console.log('Render frame');
+    }
+
+    resize() {
+        // Will be implemented in Step 2
+        console.log('Renderer resize called');
+    }
+}
