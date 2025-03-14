ab5c3d8b626f08671f47b71999d50289b7048fcb
kjigian <kjigian@gmail.com>
2025-03-14 15:52:59 -0400

    Add PhysicsEngine.js - Matter.js physics

---
+++ src/physics/PhysicsEngine.js
@@ -0,0 +1,18 @@
+/**
+ * Handles all Matter.js physics functionality for the game
+ */
+export default class PhysicsEngine {
+    constructor() {
+        console.log('PhysicsEngine initialized');
+    }
+
+    init() {
+        // Will be implemented in Step 3
+        console.log('PhysicsEngine init called');
+    }
+
+    update() {
+        // Will be implemented in Step 3
+        // console.log('Physics update');
+    }
+}
